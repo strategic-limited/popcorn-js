ed6daa9e52630c781c781b7fd620daf1828202f1
Eugene Butusov <EvgenyButusov@gmail.com>
2017-11-30 14:25:02 +0300

    Implement prototype for 360 video player

--- wrappers/vrview/popcorn.HTMLVRViewVideoElement.js
+++ wrappers/vrview/popcorn.HTMLVRViewVideoElement.js
@@ -115,9 +115,7 @@
       // We can't easily determine canplaythrough, but will send anyway.
       impl.readyState = self.HAVE_ENOUGH_DATA;
       self.dispatchEvent("canplaythrough");
-      if (!isMobile()) {
-        player.pause();
-      }
+      self.pause();
     }
 
     function handleMouseUp() {
