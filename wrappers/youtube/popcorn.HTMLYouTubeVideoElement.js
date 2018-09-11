e82763970c07f792f28681a8d171172b324baef9
Eugene Butusov <EvgenyButusov@gmail.com>
2018-09-11 23:10:16 +0300

    Play/pause for iOS is not working properly

--- wrappers/youtube/popcorn.HTMLYouTubeVideoElement.js
+++ wrappers/youtube/popcorn.HTMLYouTubeVideoElement.js
@@ -419,8 +419,10 @@
       mobileTapFix.style.bottom = '0';
       mobileTapFix.style.zIndex = '99999';
       mobileTapFix.addEventListener('click', function (e) {
-        player[player.getPlayerState() === YT.PlayerState.PAUSED ? 'playVideo' : 'pauseVideo']();
-        e.preventDefault();
+        if ([YT.PlayerState.PAUSED, YT.PlayerState.PLAYING].indexOf(player.getPlayerState()) !== -1) {
+          player[player.getPlayerState() === YT.PlayerState.PAUSED ? 'playVideo' : 'pauseVideo']();
+          e.preventDefault();
+        }
       });
       parent.appendChild(mobileTapFix);
 
