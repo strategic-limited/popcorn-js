4159bead44b5b3d2dbd974beb8107fe5c7cf1692
Eugene Butusov <EvgenyButusov@gmail.com>
2018-12-15 00:27:43 +0300

    Support adaptive playback for 360 player

--- wrappers/adaptive/popcorn.HTMLAdaptiveMediaElement.js
+++ wrappers/adaptive/popcorn.HTMLAdaptiveMediaElement.js
@@ -182,7 +182,7 @@
                 break;
             }
           } else {
-            setRawSource(fallbackMedia);
+            setRawSource(fallbackMedia || aSrc);
           }
         }
       }
