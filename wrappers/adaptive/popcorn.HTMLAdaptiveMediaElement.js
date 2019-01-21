7c9d4a03ce9068034816f265deb9cd8fb5c2d79b
Katerina <ekaterina.maksimlyuk@vedidev.com>
2019-01-21 13:48:10 +0700

    #882523780845215 Improve autoplay behaviour
    removed autoplay

--- wrappers/adaptive/popcorn.HTMLAdaptiveMediaElement.js
+++ wrappers/adaptive/popcorn.HTMLAdaptiveMediaElement.js
@@ -94,7 +94,6 @@
     media.setAttribute('playsinline', '');
     media.setAttribute('webkit-playsinline', '');
     media.setAttribute('muted', true);
-    media.setAttribute('autoplay', true);
     media.muted = true;
 
     var source = document.createElement('source');
