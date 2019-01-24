42faa53759301b01873c70191c08f9e3f3c501d6
Eugene Butusov <EvgenyButusov@gmail.com>
2019-01-24 16:44:25 +0300

    Adjust ABR rules for Dash.js

--- wrappers/adaptive/popcorn.HTMLAdaptiveMediaElement.js
+++ wrappers/adaptive/popcorn.HTMLAdaptiveMediaElement.js
@@ -177,10 +177,19 @@
                                     });
 
                                     player.on(dashjs.MediaPlayer.events.PLAYBACK_METADATA_LOADED, function () {
-                                        player.setAutoSwitchQualityFor('video', true);
-                                        player.setABRStrategy('abrThroughput');
-                                    });
+                                        var bitrates = player.getBitrateInfoListFor("video"),
+                                            // bitrates are sorted from lowest to the best values
+                                            // so the last one has the best quality
+                                            maxQuality = bitrates[bitrates.length - 1].qualityIndex;
+                                        // set max quality
+                                        player.setAutoSwitchQualityFor('video', false);
+                                        player.setQualityFor("video", maxQuality);
+                                        setTimeout(function() {
+                                            player.setAutoSwitchQualityFor('video', true);
+                                            player.setABRStrategy('abrThroughput');
+                                        }, 5000);
 
+                                    });
                                     player.initialize(media, adaptiveMedia, false);
                                 });
                                 break;
