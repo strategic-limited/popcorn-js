ce10acc60e9a38c79c4983f1fc7ea52aa062b010
Eugene Butusov <EvgenyButusov@gmail.com>
2019-09-17 15:29:05 +0300

    Remove unsupported methods for dashjs

--- wrappers/adaptive/popcorn.HTMLAdaptiveMediaElement.js
+++ wrappers/adaptive/popcorn.HTMLAdaptiveMediaElement.js
@@ -195,8 +195,6 @@
                   player.initialize(media, adaptiveMedia, false);
                   player.setTrackSwitchModeFor('video', 'alwaysReplace');
                   player.setTrackSwitchModeFor('audio', 'alwaysReplace');
-                  player.setAutoSwitchQualityFor('video', true);
-                  player.setAutoSwitchQualityFor('audio', true);
                   player.setInitialBitrateFor('video',99999999);
                   player.setInitialBitrateFor('audio',99999999);
                 });
